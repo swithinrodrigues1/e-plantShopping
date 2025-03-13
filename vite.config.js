c72f7a90a6aeb004ccb6d1744d156fed08b50599
swithinrodrigues1 <rodriguesswithin@gmail.com>
2025-03-13 09:16:40 -0400

    9

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/React-e-plantShopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 });
